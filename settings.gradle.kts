f548ddab4077cbb553eb2ea265465bc40cd0cc17
Rafał Pilecki <rafalpilecki.szczecin@gmail.com>
2026-03-25 00:41:13 +0100

    feat(Init): change project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-rootProject.name = "test"
+rootProject.name = "core_java_ravcube"
 include("lib:common")
 include("lib:data")
 include("lib:event:api")
