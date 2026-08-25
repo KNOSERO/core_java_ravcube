d51974b6cb050a9e820bbc69184814c735ea2311
Rafał Pilecki <44589200+KNOSERO@users.noreply.github.com>
2026-08-26 00:31:37 +0200

    Provide configuration properties and metrics dependencies

--- lib/stream/core/build.gradle.kts
+++ lib/stream/core/build.gradle.kts
@@ -8,6 +8,8 @@ dependencies {
     implementation(project(":lib:logger:core"))
     implementation(libs.spring.context)
     implementation(libs.spring.web)
+    implementation(libs.spring.boot.autoconfigure)
+    implementation(libs.spring.boot.actuator)
 
     testImplementation(libs.junit.jupiter)
     runtimeOnly(libs.junit.launcher)
