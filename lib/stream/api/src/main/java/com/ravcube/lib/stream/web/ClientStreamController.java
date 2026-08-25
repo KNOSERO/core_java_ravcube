f99eec74cdaf8e236c518692ea5bdf8b776e082e
Rafał Pilecki <44589200+KNOSERO@users.noreply.github.com>
2026-08-25 23:31:16 +0200

    Fix Spring placeholder syntax

--- lib/stream/api/src/main/java/com/ravcube/lib/stream/web/ClientStreamController.java
+++ lib/stream/api/src/main/java/com/ravcube/lib/stream/web/ClientStreamController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Objects;
 
 @RestController
-@RequestMapping("\${ravcube.stream.path:/streams}")
+@RequestMapping("${ravcube.stream.path:/streams}")
 public final class ClientStreamController {
 
     private final ClientStreamService service;
