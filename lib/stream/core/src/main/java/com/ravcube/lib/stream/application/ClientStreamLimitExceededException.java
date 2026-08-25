400a7aaa50d197bd9b506f82beb94b63871c0e13
Rafał Pilecki <44589200+KNOSERO@users.noreply.github.com>
2026-08-25 23:30:04 +0200

    Hide stream implementation details from public modules

--- lib/stream/core/src/main/java/com/ravcube/lib/stream/application/ClientStreamLimitExceededException.java
+++
@@ -1,8 +0,0 @@
-package com.ravcube.lib.stream.application;
-
-public final class ClientStreamLimitExceededException extends RuntimeException {
-
-    public ClientStreamLimitExceededException(String message) {
-        super(message);
-    }
-}
