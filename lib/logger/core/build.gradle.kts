6e3b7ab0ac8108c5a7e2afbee5de0f5b3424006d
Rafał Pilecki <44589200+KNOSERO@users.noreply.github.com>
2026-08-25 23:37:54 +0200

    Declare logger runtime facade dependency

--- lib/logger/core/build.gradle.kts
+++ lib/logger/core/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     api(project(":lib:logger:api"))
     implementation(libs.spring.context)
+    implementation(libs.spring.jcl)
 
     testImplementation(libs.junit.jupiter)
     runtimeOnly(libs.junit.launcher)
