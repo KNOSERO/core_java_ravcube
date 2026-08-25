10a01f35518c6dd7e2a773f444db6ba0068334fb
Rafał Pilecki <44589200+KNOSERO@users.noreply.github.com>
2026-08-25 21:34:11 +0200

    Remove unused stream authorization contract

--- lib/stream/core/src/main/java/com/ravcube/lib/stream/application/ClientStreamAccessDeniedException.java
+++
@@ -1,8 +0,0 @@
-package com.ravcube.lib.stream.application;
-
-public final class ClientStreamAccessDeniedException extends RuntimeException {
-
-    public ClientStreamAccessDeniedException(String resourceName, String resourceId) {
-        super("Access denied for stream resource " + resourceName + "/" + resourceId);
-    }
-}
